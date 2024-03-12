fda70b12fdf7915aa162014018e0de22ae7a6020
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-03-12 17:35:37 +0530

    Added open method to result modal and detached dialog box from Timer challenge

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,14 +1,24 @@
-export default function ResultModal({ result, targetTime }) {
+import { forwardRef, useImperativeHandle, useRef } from "react"
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
     return (
-        <dialog className="result-modal" open>
-            <h2>
-                You {result}!!!
-            </h2>
+        <dialog ref={dialog} className="result-modal" >
+            <h2>You {result}!!!</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds.</strong></p>
-            <form action="" method="dialog">
+            <form method="dialog">
                 <button>Close</button>
             </form>
         </dialog>
     )
-}
+});
+
+export default ResultModal;
